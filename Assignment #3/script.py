04a134e64a4fe53204e3d939dadd3d57671116bb
Maryam <mkaka139@gmail.com>
2017-02-21 23:36:58 -0500

    Completed Question 1

--- Assignment #3/script.py
+++ Assignment #3/script.py
@@ -1,24 +1,86 @@
 import pandas as pd
-import numpy
+import numpy as np
+import scipy.stats as stats
+import math
 import matplotlib.pyplot as plt
 import bisect
 from sklearn import metrics
 
 outputLocation = 'Assignment #3/images/'
 
+def chiSquared(data):
+    df = ((data.shape[0] - 1) - 1) * ((data.shape[1] - 1) - 1)
+    contingencyTable = np.outer(data.RowTotal[0:-1], data.ix["ColTotal"][0:-1])\
+        /data.ix["ColTotal","RowTotal"]
+    contingencyTable = pd.DataFrame(contingencyTable, index=rows, columns = col)
+
+    x, p = stats.chisquare(data.ix[0:-1, 0:-1], contingencyTable, ddof = df)
+    x = x.sum()
+    p = 1 - stats.chi2.cdf(x, df)
+
+    print('Degrees of Freedom: ' + str(df))
+    print("Chi^2: " + str(x))
+    print("p-value: " + str(p))
+
+def confusionMatrix(tp, fn, fp, tn):
+    data = pd.DataFrame([[tp, fn], [fp, tn]], index=rows, columns=col)
+    data = data.append(pd.Series(data.sum(), name="ColTotal"))
+    data = data.assign(RowTotal=pd.Series(data.sum(axis=1)))
+    print(data)
+
+    return data
+
 print("QUESTION 1")
+col = ["TestT", "TestF"]
+rows = ["ActualT", "ActualF"]
+
 SENS = 0.65
 SPEC = 0.62
-nPos = 100
-nNeg = 100
-tp = SENS
-fn = nPos - SENS
-fp = nNeg - SPEC
-tn = SPEC
 
-print('QUESTION 2')
-data = pd.read_csv("Assignment #3/assigData3.tsv", sep='\t', index_col=False, header=None,
-    names=["score", "class"])
+ppv = lambda tp, fp: tp/(tp+fp)
+
+print('(i)')
+print('a.')
+nPos = 100; nNeg = 100
+tp = SENS*nPos; fn = nPos - tp; tn = SPEC*nNeg; fp = nNeg - tn
+PPVa = ppv(tp, fp)
+chiSquared(confusionMatrix(tp, fn, fp, tn))
+
+print('\nb.')
+nPos = 100; nNeg = 1000
+tp = SENS*nPos; fn = nPos - tp; tn = SPEC*nNeg; fp = nNeg - tn
+PPVb = ppv(tp, fp)
+chiSquared(confusionMatrix(tp, fn, fp, tn))
+
+print('\nc.')
+nPos = 400; nNeg = 400
+tp = SENS*nPos; fn = nPos - tp; tn = SPEC*nNeg; fp = nNeg - tn
+PPVc = ppv(tp, fp)
+chiSquared(confusionMatrix(tp, fn, fp, tn))
+
+print('\n(ii)')
+print('a. PPV: ' + str(PPVa))
+print('b. PPV: ' + str(PPVb))
+print('c. PPV: ' + str(PPVc))
+
+plt.figure()
+plt.plot(0.5, 0.5, marker='P', label='Random Classifier')
+plt.plot(SENS, PPVa, marker='o', label='a')
+plt.plot(SENS, PPVb, marker='s', label='b')
+plt.plot(SENS, PPVc, marker='x', label='c')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.title('Precision Recall Curve')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.legend(loc = 'lower left')
+plt.grid(True)
+plt.savefig(outputLocation + 'precision-recall.png', bbox_inches='tight')
+
+
+print('\nQUESTION 2')
+data = pd.read_csv("Assignment #3/assigData3.tsv", sep='\t', index_col=False,
+    header=None, names=["score", "class"])
 
 print('(i)')
 fpr, tpr, thresh = metrics.roc_curve(data['class'], data['score'], pos_label=1)
@@ -41,17 +103,19 @@ index = bisect.bisect_left(tpr, minSens)
 print('Max Specificity: ' + str(1-fpr[index]))
 
 print('(iii)')
-precision, recall, t = metrics.precision_recall_curve(data['class'], data['score'], pos_label=1)
+precision, recall, t = metrics.precision_recall_curve(data['class'],
+    data['score'], pos_label=1)
 auc = metrics.auc(recall[1:-1], precision[1:-1])
 
 plt.figure()
-plt.plot(recall[1:-1], precision[1:-1], label='Precision-Recall Curve (AUC = %0.2f)' %auc)
+plt.plot(recall[1:-1], precision[1:-1],
+    label='Precision-Recall Curve (AUC = %0.2f)' %auc)
 plt.title('Precision Recall Curve')
 plt.xlabel('Recall')
 plt.ylabel('Precision')
 plt.legend(loc = 'lower right')
 plt.grid(True)
-plt.savefig(outputLocation + 'precision-recall.png', bbox_inches='tight')
+plt.savefig(outputLocation + 'precision-recall2.png', bbox_inches='tight')
 
 print('(v)')
 index = index = [i for i,x in enumerate(recall) if x == 0.75][0]
